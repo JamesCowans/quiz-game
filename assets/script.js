cfecd31c6b6a902bb98223398af867319e935c14
JamesCowans <>
2023-09-09 18:11:48 +0000

    add first question in the javascript code and add placeholders for required function

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,54 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function ());
+    } )
+})
+
+
+
+
+
+
 function topicSelect() {
 
 }
 
+const sportsQuestion = [
+    {
+        Question 1: 'Who is the record goal scorer for Newcastle United?',
+        Answer: [{ text: = 'Alan Shearer', correct: true },
+        { text: = 'Stephane Guivarc\'h', correct: false },
+        { text: = 'Boris Johnson', correct: false },
+        { text: = 'Sherlock Holmes', correct: false }
+
+        ]
+
 function topicSports() {
+        Question1 =
 
 }
 
 function topicMusic() {
 
-}
+    }
 
 function topicFilm() {
 
-}
+    }
 
 function topicRandom() {
 
-}
+    }
 
 function checkCorrectAnswer() {
 
-}
+    }
 
 function correctScore() {
 
-}
+    };
 
 
 
