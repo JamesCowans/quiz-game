c1a80ef779722aea53ea838b65c68ab4b6d186cb
JamesCowans <>
2023-09-09 17:05:09 +0000

    remove option to enter player name

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+function topicSelect() {
+
+}
+
+function topicSports() {
+
+}
+
+function topicMusic() {
+
+}
+
+function topicFilm() {
+
+}
+
+function topicRandom() {
+
+}
+
+function checkCorrectAnswer() {
+
+}
+
+function correctScore() {
+
+}
+
+
+
